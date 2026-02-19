e510544aa8fb4cf71fa368f2a8915e85b32140de
Tenacious <tenaciousmthembu.com>
2026-02-19 03:47:23 +0200

    Added an extra /

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB()
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://blogitblogsite.vercel.app/"
+        "https://blogitblogsite.vercel.app"
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
